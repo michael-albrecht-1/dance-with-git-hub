a389e1b843d7b5af437c19d532783be8469092b1
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-04-18 15:34:20 +0200

    RepoResults add star dont awesome icon and useState

--- src/components/FavortitesRepos/index.js
+++ src/components/FavortitesRepos/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './favorites.scss';
 
 const FavoritesRepos = () => (
+  // # utilisateur : nom / id
+  // # favoritesRepos :  / id
   <div className="favorites">
     My favorites repos
   </div>
--- src/components/RepoResults/RepoResults.js
+++ src/components/RepoResults/RepoResults.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './RepoResults.scss';
 
-const RepoResults = ({ repo }) => (
-  <div className="repo">
-    <a href={repo.html_url}>
-      <div className="repo__img">
-        <img src={repo.owner.avatar_url} alt="auteur" />
-      </div>
-      <h1 className="repo__title">{repo.name}</h1>
-      <p className="repo__author">{repo.owner.login}</p>
-      <p className="repo__description">{repo.description}</p>
-    </a>
-  </div>
-);
+const RepoResults = ({ repo }) => {
+  const [isStar, setIsStar] = useState(false);
+  const handleStarClick = () => setIsStar(!isStar);
+  return (
+    <div className="repo">
+      {
+        isStar ? (
+          <i
+            className="fas fa-star repo__icon"
+            onClick={handleStarClick}
+          />
+        )
+          : (
+            <i
+              className="far fa-star repo__icon"
+              onClick={handleStarClick}
+            />
+          )
+      }
+      <a href={repo.html_url}>
+        <div className="repo__img">
+          <img src={repo.owner.avatar_url} alt="auteur" />
+        </div>
+        <h1 className="repo__title">{repo.name}</h1>
+        <p className="repo__author">{repo.owner.login}</p>
+        <p className="repo__description">{repo.description}</p>
+      </a>
+    </div>
+  );
+};
 
 RepoResults.propTypes = {
   repo: PropTypes.shape({
